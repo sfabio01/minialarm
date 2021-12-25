2cac8ecbd3a8c56170d5f1b342eaa48bb898107a
Fabio Sabbion <fabiosabbion.apps@gmail.com>
2021-12-25 23:01:40 +0100

    dev

---
+++ android/app/src/main/java/com/fabiosabbion/minimalarm/AlarmActivity.kt
@@ -0,0 +1,41 @@
+package com.fabiosabbion.minimalarm
+
+import android.app.Activity
+import android.content.Context
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.*
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class AlarmActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+
+        val closeBtn: Button = findViewById(R.id.closebtn)
+
+        val vibrate = intent.getBooleanExtra("vibrate", false)
+        val ringtoneUri = intent.getStringExtra("ringtone")
+
+        val ringtone = RingtoneManager.getRingtone(applicationContext, Uri.parse(ringtoneUri))
+        ringtone.play()
+
+        val vibrator: Any
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            vibrator = applicationContext.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibrator.vibrate(CombinedVibration.createParallel(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE)))
+
+        } else {
+            vibrator = applicationContext.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            vibrator.vibrate(500)
+
+        }
+
+        closeBtn.setOnClickListener {
+            ringtone.stop()
+        }
+
+
+    }
+}
--- android/app/src/main/kotlin/com/fabiosabbion/minimalarm/AlarmReceiver.kt
+++ android/app/src/main/kotlin/com/fabiosabbion/minimalarm/AlarmReceiver.kt
@@ -16,22 +16,25 @@ class AlarmReceiver: BroadcastReceiver(){
         val defaultRingtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM).toString()
         var vibrate = false
         var ringtoneUri  = defaultRingtoneUri
-        // val vibrator: Vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         // TODO: check if to vibrate
         Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show()
         val prefs = context?.getSharedPreferences("FlutterSharedPreferences", Context.MODE_PRIVATE)
         if (prefs != null) {
             vibrate = prefs.getBoolean(prefix+"vibrate", false)
-            ringtoneUri = prefs.getString(prefix+"sound", defaultRingtoneUri).toString()
+            // TODO ringtoneUri = prefs.getString(prefix+"sound", defaultRingtoneUri).toString()
             val editor = prefs.edit()
             editor.remove(prefix+"hour")
             editor.remove(prefix+"min")
             editor.apply()
         }
 
+        val alarmIntent = Intent(context, AlarmActivity::class.java)
+        alarmIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        alarmIntent.putExtra("vibrate", vibrate)
+        alarmIntent.putExtra("ringtone", ringtoneUri)
+        context?.startActivity(alarmIntent)
 
-        // val ringtone = RingtoneManager.getRingtone(context, Uri.parse(ringtoneUri))
-        // ringtone.play()
+
 
 
     }
